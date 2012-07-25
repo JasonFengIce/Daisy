9e9658f4c33d1eca369ac1c8126f7453f47bc627
unknown <wlt@.(none)>
2012-07-26 01:51:14 +0800

    build folder structure

---
+++ src/tv/ismar/daisy/MainApplication.java
@@ -0,0 +1,14 @@
+package tv.ismar.daisy;
+
+import com.googlecode.androidannotations.annotations.EApplication;
+
+import android.app.Application;
+
+
+@EApplication
+public class MainApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+     }
+
+}
---
+++ src/tv/ismar/daisy/views/PlayerActivity.java
@@ -0,0 +1,27 @@
+package tv.ismar.daisy.views;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.NavUtils;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_channel)
+public class PlayerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void onBackPressed() {
+        Toast.makeText(this, "Back key pressed!", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
+
