15da4b579a6e2fc0aabe4035498dfa71540ad489
liuhao <liuhaoliter@gmail.com>
2012-08-06 14:21:38 +0800

    edit name

--- src/tv/ismar/daisy/PlayFinishedActivity.java
+++ src/tv/ismar/daisy/PlayFinishedActivity.java
@@ -31,7 +31,6 @@ public class PlayFinishedActivity extends Activity implements OnFocusChangeListe
 	private Bitmap bitmap;
 	LinearLayout linearLeft;
 	LinearLayout linearRight;
-
 	TextView tvVodName;
 	ImageView imageBackgroud;
 	ImageView imageVodLabel;
