a2f1fc92f26b2a0ff3e0e35fa580b8241cb4f78c
Yuanbo <yuanbo@ismartv.cn>
2012-08-07 14:02:49 +0800

    nothing changed

--- src/tv/ismar/daisy/views/ItemListContainer.java
+++ src/tv/ismar/daisy/views/ItemListContainer.java
@@ -2,16 +2,22 @@ package tv.ismar.daisy.views;
 
 import tv.ismar.daisy.R;
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.FloatMath;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class ItemListContainer extends LinearLayout {
 	
+	private static final String TAG = "";
+	
 	private static final int DEFAULT_ROWS = 3;
 	
 	private int mTotalCellCount;
+	
+	private Rect mTempRect;
 
 	public ItemListContainer(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -72,5 +78,4 @@ public class ItemListContainer extends LinearLayout {
 		return index - (index / DEFAULT_ROWS)*DEFAULT_ROWS;
 	}
 
-	
 }
