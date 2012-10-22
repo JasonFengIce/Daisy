011721f6e8d129dd89359091ae86c0e4226eed23
liuhao <liuhaoliter@gmail.com>
2012-10-22 19:02:09 +0800

    add

--- src/tv/ismar/daisy/SearchActivity.java
+++ src/tv/ismar/daisy/SearchActivity.java
@@ -251,6 +251,8 @@ public class SearchActivity extends Activity implements OnClickListener, OnItemC
 				// autoAdapter.notifyDataSetChanged();
 				break;
 			case ADD_VIEW:
+				if (null == listHotWords) 
+					return;
 				for (int j = 0; j < listHotWords.size(); j++) {
 					final Button btnHotWords = new Button(SearchActivity.this);
 					// final Button btnHotWords = (Button) findViewById(R.id.btn_words);
