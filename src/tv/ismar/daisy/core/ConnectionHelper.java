aa46caebdf6f2bd49c8d8b54831170b74b0d8edf
liuhao <liuhaoliter@gmail.com>
2012-11-20 18:26:31 +0800

    add

--- src/tv/ismar/daisy/core/ConnectionHelper.java
+++ src/tv/ismar/daisy/core/ConnectionHelper.java
@@ -15,8 +15,14 @@ public class ConnectionHelper {
 	public static boolean isNetWorkAvailable(Context context) {
 		ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		if (connectivity != null) {
-			NetworkInfo netWorkInfo = connectivity.getActiveNetworkInfo();
-			return netWorkInfo != null && netWorkInfo.isAvailable();
+			NetworkInfo[] info = connectivity.getAllNetworkInfo();
+			if (info != null) {
+				for (int i = 0; i < info.length; i++) {
+					if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+						return true;
+					}
+				}
+			}
 		}
 		return false;
 	}
