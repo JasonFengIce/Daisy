0c8268db6605335b8560085251acbd2f454a9e6a
zhangjiqiang <zhangjiqiang@ismartv.cn>
2014-09-25 17:42:56 +0800

    event property

---
+++ src/tv/ismar/daisy/core/EventProperty.java
@@ -0,0 +1,26 @@
+package tv.ismar.daisy.core;
+
+public class EventProperty {
+
+	/**
+	 * video_Property
+	 */
+	public final static String ITEM = "item";
+	public final static String SUBITEM = "subitem";
+	public final static String CLIP = "clip";
+	public final static String TITLE = "title";
+	public final static String QUALITY = "quality";
+	public final static String TO = "to";
+	public final static String DURATION = "duration";
+	public final static String POSITION = "position";
+	public final static String SPEED = "speed";
+	public final static String SID = "sid";
+	public final static String SECTION = "section";
+	public final static String CHANNEL = "channel";
+	public final static String SOURCE = "source";
+	public final static String LOCATION = "location";
+	public final static String CODE = "code";
+	public final static String CONTENT = "content";
+	public final static String MEDIAIP = "mediaip";
+	public final static String MODE = "mode";
+}
