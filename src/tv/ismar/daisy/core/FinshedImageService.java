f3d4a1e7a47885517fc5d8df428b0e3a3cf893ea
zhangjiqiang <zhangjiqiang@ismartv.cn>
2015-01-08 17:53:45 +0800

    720dp hdpi xhdpi

--- src/tv/ismar/daisy/core/FinshedImageService.java
+++ src/tv/ismar/daisy/core/FinshedImageService.java
@@ -10,11 +10,15 @@ public class FinshedImageService {
 	public static Object[] getImage(Item item,Context context) {
 		Object[] object = new Object[2];
 		int resourceLabel = 0;
-		int H = DaisyUtils.getVodApplication(context).getheightPixels(context);
-		if(H==720||(H>720&&H<1080))
-			object[0] = ImageUtils.getBitmapFromInputStream(NetworkUtils.getInputStream(item.adlet_url), (int)(188), (int)(106));
-		else
-			object[0] = ImageUtils.getBitmapFromInputStream(NetworkUtils.getInputStream(item.adlet_url), (int)(282), (int)(158));
+//		int H = DaisyUtils.getVodApplication(context).getheightPixels(context);
+//		if(H==720||(H>720&&H<1080))
+//			object[0] = ImageUtils.getBitmapFromInputStream(NetworkUtils.getInputStream(item.adlet_url), (int)(188), (int)(106));
+//		else
+//			object[0] = ImageUtils.getBitmapFromInputStream(NetworkUtils.getInputStream(item.adlet_url), (int)(282), (int)(158));
+		
+		object[0] = ImageUtils.getBitmapFromInputStream(NetworkUtils.getInputStream(item.adlet_url), 
+				context.getResources().getDimensionPixelSize(R.dimen.play_finished_image_vodie_backgroud_W), 
+				context.getResources().getDimensionPixelSize(R.dimen.play_finished_image_vodie_backgroud_H));
 //		object[0] = (BitmapFactory.decodeStream(HttpUtil.getHttpConnectionByGet(item.adlet_url).getInputStream()));
 		switch (item.quality) {
 		case 3:
