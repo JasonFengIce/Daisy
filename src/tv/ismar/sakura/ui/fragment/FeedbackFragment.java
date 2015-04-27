978819833432f657a0677af2c644045af7c29887
fenghuibin <fenghuibin@ismartv.cn>
2015-04-27 16:11:56 +0800

    modify feedback fragment text is phone number or not

--- src/tv/ismar/sakura/ui/fragment/FeedbackFragment.java
+++ src/tv/ismar/sakura/ui/fragment/FeedbackFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -28,6 +29,8 @@ import tv.ismar.sakura.ui.widget.MessageSubmitButton;
 import tv.ismar.sakura.ui.widget.SakuraEditText;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static tv.ismar.sakura.core.SakuraClientAPI.restAdapter_IRIS_TVXIO;
 
@@ -130,25 +133,79 @@ public class FeedbackFragment extends Fragment implements RadioGroup.OnCheckedCh
     }
 
     private void uploadFeedback() {
-        IpLookUpCache ipLookUpCache = IpLookUpCache.getInstance(getActivity());
-        FeedBackEntity feedBack = new FeedBackEntity();
-        feedBack.setDescription(descriptioinText.getText().toString());
-        feedBack.setPhone(phoneNumberText.getText().toString());
-        feedBack.setOption(problemTextFlag);
-        feedBack.setCity(ipLookUpCache.getUserCity());
-        feedBack.setIp(ipLookUpCache.getUserIp());
-        feedBack.setIsp(ipLookUpCache.getUserIsp());
-        feedBack.setLocation(ipLookUpCache.getUserProvince());
-        UploadFeedback.getInstance().excute(feedBack, snCode, new UploadFeedback.Callback() {
-            @Override
-            public void success(String msg) {
-                Log.d(TAG, "uploadFeedback: " + msg);
-            }
 
-            @Override
-            public void failure(String msg) {
-                Log.d(TAG, "uploadFeedback: " + msg);
-            }
-        });
+
+        String contactNumber = phoneNumberText.getText().toString();
+        if (TextUtils.isEmpty(contactNumber)) {
+            submitButton.setEnabled(true);
+            Toast.makeText(getActivity(), R.string.fill_contact_number, Toast.LENGTH_LONG).show();
+
+        } else if ((!isMobile(contactNumber) && !isPhone(contactNumber))) {
+            submitButton.setEnabled(true);
+            Toast.makeText(getActivity(), R.string.you_should_give_an_phone_number, Toast.LENGTH_LONG).show();
+
+        } else {
+
+            IpLookUpCache ipLookUpCache = IpLookUpCache.getInstance(getActivity());
+            FeedBackEntity feedBack = new FeedBackEntity();
+            feedBack.setDescription(descriptioinText.getText().toString());
+            feedBack.setPhone(contactNumber);
+            feedBack.setOption(problemTextFlag);
+            feedBack.setCity(ipLookUpCache.getUserCity());
+            feedBack.setIp(ipLookUpCache.getUserIp());
+            feedBack.setIsp(ipLookUpCache.getUserIsp());
+            feedBack.setLocation(ipLookUpCache.getUserProvince());
+            UploadFeedback.getInstance().excute(feedBack, snCode, new UploadFeedback.Callback() {
+                @Override
+                public void success(String msg) {
+                    Log.d(TAG, "uploadFeedback: " + msg);
+                }
+
+                @Override
+                public void failure(String msg) {
+                    Log.d(TAG, "uploadFeedback: " + msg);
+                }
+            });
+        }
     }
+
+
+    /**
+     * 手机号验证
+     *
+     * @param str
+     * @return 验证通过返回true
+     */
+    public static boolean isMobile(String str) {
+        Pattern p = null;
+        Matcher m = null;
+        boolean b = false;
+        p = Pattern.compile("^[1][3,4,5,8][0-9]{9}$"); // 验证手机号
+        m = p.matcher(str);
+        b = m.matches();
+        return b;
+    }
+
+    /**
+     * 电话号码验证
+     *
+     * @param str
+     * @return 验证通过返回true
+     */
+    public static boolean isPhone(String str) {
+        Pattern p1 = null, p2 = null;
+        Matcher m = null;
+        boolean b = false;
+        p1 = Pattern.compile("^[0][0-9]{2,3}-[0-9]{5,10}$");  // 验证带区号的
+        p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");         // 验证没有区号的
+        if (str.length() > 9) {
+            m = p1.matcher(str);
+            b = m.matches();
+        } else {
+            m = p2.matcher(str);
+            b = m.matches();
+        }
+        return b;
+    }
+
 }
