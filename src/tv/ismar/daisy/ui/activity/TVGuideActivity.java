a64dc8d775f6d16e0590fa8a84414470584c0c7d
fenghuibin <fenghuibin@ismartv.cn>
2015-05-19 16:57:03 +0800

    add horizontal list view

--- src/tv/ismar/daisy/ui/activity/TVGuideActivity.java
+++ src/tv/ismar/daisy/ui/activity/TVGuideActivity.java
@@ -14,10 +14,14 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 import tv.ismar.daisy.AppConstant;
 import tv.ismar.daisy.R;
+import tv.ismar.daisy.adapter.ChannleListAdapter;
 import tv.ismar.daisy.core.SimpleRestClient;
 import tv.ismar.daisy.core.client.ClientApi;
 import tv.ismar.daisy.ui.fragment.*;
 import tv.ismar.daisy.ui.widget.ChannelGridView;
+import tv.ismar.daisy.ui.widget.HorizontalListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by huaijie on 5/18/15.
@@ -31,8 +35,10 @@ public class TVGuideActivity extends FragmentActivity {
     public static final String TAG_FILM_FRAGMENT = "film";
     public static final String TAG_SPORT_FRAGMENT = "sport";
 
+    private ChildFragmentContainer childFragmentContainer;
 
-    private ChannelGridView channelGrid;
+
+//    private ChannelGridView channelGrid;
 
     private Button change;
 
@@ -41,6 +47,7 @@ public class TVGuideActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_guide);
+        childFragmentContainer = new ChildFragmentContainer();
 
         if (savedInstanceState == null) {
             final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -48,47 +55,56 @@ public class TVGuideActivity extends FragmentActivity {
         } else {
 
         }
-        channelGrid = (ChannelGridView)findViewById(R.id.channel_grid);
+//        channelGrid = (ChannelGridView) findViewById(R.id.channel_grid);
 
 
+//        fetchChannels();
 
-        fetchChannels();
+        HorizontalListView horizontalListView = (HorizontalListView) findViewById(R.id.channel_h_list);
 
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("channel --- " + i);
+        }
+
+        ChannleListAdapter channleListAdapter = new ChannleListAdapter(this, list);
+        horizontalListView.setAdapter(channleListAdapter);
 
         change = (Button) findViewById(R.id.change);
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.container, new ChildFragmentContainer(), TAG_ENTERTAINMENT_FRAGMENT).commit();
+                if (!childFragmentContainer.isVisible()) {
+                    final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.container, childFragmentContainer, TAG_ENTERTAINMENT_FRAGMENT).commit();
+                }
             }
         });
     }
-
 
 
     /**
      * fetch channel
      */
-    private void fetchChannels() {
-        String deviceToken = SimpleRestClient.device_token;
-        String host = SimpleRestClient.root_url;
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setLogLevel(AppConstant.LOG_LEVEL)
-                .setEndpoint(host)
-                .build();
-        ClientApi.Channels client = restAdapter.create(ClientApi.Channels.class);
-        client.excute(deviceToken, new Callback<ChannelEntity[]>() {
-            @Override
-            public void success(ChannelEntity[] channelEntities, Response response) {
-                channelGrid.setAdapter(channelEntities);
-            }
-
-            @Override
-            public void failure(RetrofitError retrofitError) {
-                Log.e(TAG, retrofitError.getMessage());
-            }
-        });
-    }
+//    private void fetchChannels() {
+//        String deviceToken = SimpleRestClient.device_token;
+//        String host = SimpleRestClient.root_url;
+//        RestAdapter restAdapter = new RestAdapter.Builder()
+//                .setLogLevel(AppConstant.LOG_LEVEL)
+//                .setEndpoint(host)
+//                .build();
+//        ClientApi.Channels client = restAdapter.create(ClientApi.Channels.class);
+//        client.excute(deviceToken, new Callback<ChannelEntity[]>() {
+//            @Override
+//            public void success(ChannelEntity[] channelEntities, Response response) {
+//                channelGrid.setAdapter(channelEntities);
+//            }
+//
+//            @Override
+//            public void failure(RetrofitError retrofitError) {
+//                Log.e(TAG, retrofitError.getMessage());
+//            }
+//        });
+//    }
 
 }
