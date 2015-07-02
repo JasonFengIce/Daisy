6bddf0b7c858b635126643b903571b13652a72dd
fenghuibin <fenghuibin@ismartv.cn>
2015-07-02 10:24:45 +0800

    add thumb image

---
+++ src/tv/ismar/daisy/ui/widget/ThumbImageView.java
@@ -0,0 +1,18 @@
+package tv.ismar.daisy.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by huaijie on 7/1/15.
+ */
+public class ThumbImageView extends RelativeLayout {
+    public ThumbImageView(Context context) {
+        super(context);
+    }
+
+    public ThumbImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
