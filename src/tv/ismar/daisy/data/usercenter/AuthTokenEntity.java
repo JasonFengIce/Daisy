31b02172c52b51be8be8c7f6b49c31702d46073f
fenghuibin <fenghuibin@ismartv.cn>
2015-07-09 16:14:09 +0800

    modify gitignore file

---
+++ src/tv/ismar/daisy/data/usercenter/AuthTokenEntity.java
@@ -0,0 +1,16 @@
+package tv.ismar.daisy.data.usercenter;
+
+/**
+ * Created by huaijie on 7/9/15.
+ */
+public class AuthTokenEntity {
+    private String auth_token;
+
+    public String getAuth_token() {
+        return auth_token;
+    }
+
+    public void setAuth_token(String auth_token) {
+        this.auth_token = auth_token;
+    }
+}
