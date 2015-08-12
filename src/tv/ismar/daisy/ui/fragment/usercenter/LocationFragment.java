0bddb9de8d6fdfb0a49bb721d93533f9bf64809b
fenghuibin <fenghuibin@ismartv.cn>
2015-08-12 17:29:31 +0800

    modify usercenter weather

--- src/tv/ismar/daisy/ui/fragment/usercenter/LocationFragment.java
+++ src/tv/ismar/daisy/ui/fragment/usercenter/LocationFragment.java
@@ -9,10 +9,12 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import com.activeandroid.query.Select;
+import org.w3c.dom.Text;
 import tv.ismar.daisy.R;
 import tv.ismar.daisy.core.preferences.AccountSharedPrefs;
 import tv.ismar.daisy.data.table.location.CityTable;
@@ -40,18 +42,18 @@ public class LocationFragment extends Fragment implements ProvinceAdapter.OnItem
 
 
     private TextView currentPostion;
+    private TextView selectedPositionTitle;
+    private TextView selectedPosition;
+
+
+    private int selectedAreaPositon;
+    private TextView selectedAreaTextView;
 
     private SharedPreferences.OnSharedPreferenceChangeListener changeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            String stringRes = mContext.getString(R.string.location_current_position);
-//            String geoId = sharedPreferences.getString(LOCATION_PREFERENCE_GEOID, "101020100");
-//
-//            LocationTable locationTable = new Select().from(LocationTable.class).where("geo_id=?", geoId).executeSingle();
-//            if (null != locationTable) {
             String cityName = AccountSharedPrefs.getInstance(mContext).getSharedPrefs(AccountSharedPrefs.CITY);
-            currentPostion.setText(String.format(stringRes, cityName));
-//            }
+            currentPostion.setText(cityName);
         }
     };
 
@@ -72,8 +74,10 @@ public class LocationFragment extends Fragment implements ProvinceAdapter.OnItem
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         fragmentView = inflater.inflate(R.layout.fragment_location, null);
-        currentPostion = (TextView) fragmentView.findViewById(R.id.current_position);
+        currentPostion = (TextView) fragmentView.findViewById(R.id.currentPosition);
+        selectedPosition = (TextView) fragmentView.findViewById(R.id.selectedPosition);
         provinceListView = (GridView) fragmentView.findViewById(R.id.province_list);
+        selectedPositionTitle  =(TextView)fragmentView.findViewById(R.id.selectedPosition_title);
 
         return fragmentView;
     }
@@ -81,12 +85,11 @@ public class LocationFragment extends Fragment implements ProvinceAdapter.OnItem
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        String stringRes = mContext.getString(R.string.location_current_position);
         String cityName = AccountSharedPrefs.getInstance(mContext).getSharedPrefs(AccountSharedPrefs.CITY);
 
 //        CityTable cityTable = new Select().from(CityTable.class).where(CityTable.CITY + " = ?", cityName).executeSingle();
 //        if (null != cityTable) {
-        currentPostion.setText(String.format(stringRes, cityName));
+        currentPostion.setText(cityName);
 //        }
 
     }
@@ -112,6 +115,10 @@ public class LocationFragment extends Fragment implements ProvinceAdapter.OnItem
         String provinceId = provinceTable.province_id;
         View popupLayout = LayoutInflater.from(mContext).inflate(R.layout.popup_area, null);
         GridView gridView = (GridView) popupLayout.findViewById(R.id.area_grid);
+        final Button confirmBtn = (Button) popupLayout.findViewById(R.id.confirm_btn);
+        final Button cancelBtn = (Button) popupLayout.findViewById(R.id.cancel_btn);
+        final TextView selectPrompt = (TextView) popupLayout.findViewById(R.id.area_select_prompt);
+
 
         int width = (int) mContext.getResources().getDimension(R.dimen.location_area_pop_width);
         int height = (int) mContext.getResources().getDimension(R.dimen.location_area_pop_height);
@@ -124,10 +131,52 @@ public class LocationFragment extends Fragment implements ProvinceAdapter.OnItem
 
         final List<CityTable> locationTableList = new Select().from(CityTable.class).where(CityTable.PROVINCE_ID + " = ?", provinceId).execute();
         CityAdapter cityAdapter = new CityAdapter(mContext, locationTableList);
+
+
         cityAdapter.setOnItemListener(new OnItemListener() {
             @Override
             public void onClick(View view, int position) {
-                String city = locationTableList.get(position).city;
+                if (null != selectedAreaTextView) {
+                    selectedAreaTextView.setTextColor(mContext.getResources().getColor(R.color.white));
+                    selectedAreaTextView.setTextSize(mContext.getResources().getDimension(R.dimen.h2_text_size));
+                }
+                selectedAreaPositon = position;
+                selectedAreaTextView = (TextView) view;
+                String city = locationTableList.get(selectedAreaPositon).city;
+                {
+                    selectedPosition.setVisibility(View.VISIBLE);
+                    selectedPositionTitle.setVisibility(View.VISIBLE);
+                    selectPrompt.setVisibility(View.VISIBLE);
+                    confirmBtn.setVisibility(View.VISIBLE);
+                    cancelBtn.setVisibility(View.VISIBLE);
+
+                }
+                selectedPosition.setText(city);
+                selectedAreaTextView.setTextColor(mContext.getResources().getColor(R.color.blue));
+                selectedAreaTextView.setTextSize(mContext.getResources().getDimension(R.dimen.h1_text_size));
+            }
+
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                TextView textView = (TextView) v;
+                if (hasFocus) {
+                    textView.setTextColor(mContext.getResources().getColor(R.color.location_text_focus));
+                    textView.setTextSize(mContext.getResources().getDimension(R.dimen.h1_text_size));
+                } else {
+                    if (selectedAreaTextView != v) {
+                        textView.setTextColor(mContext.getResources().getColor(R.color.white));
+                        textView.setTextSize(mContext.getResources().getDimension(R.dimen.h2_text_size));
+                    }
+
+                }
+            }
+        });
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = locationTableList.get(selectedAreaPositon).city;
                 AccountSharedPrefs accountSharedPrefs = AccountSharedPrefs.getInstance(mContext);
                 accountSharedPrefs.setSharedPrefs(AccountSharedPrefs.CITY, city);
                 accountSharedPrefs.setSharedPrefs(AccountSharedPrefs.PROVINCE, provinceTable.province_name);
@@ -138,19 +187,26 @@ public class LocationFragment extends Fragment implements ProvinceAdapter.OnItem
                 }
                 areaPopup.dismiss();
             }
+        });
 
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                TextView textView = (TextView) v;
-                if (hasFocus) {
-                    textView.setTextColor(mContext.getResources().getColor(R.color.location_text_focus));
-                    textView.setTextSize(mContext.getResources().getDimension(R.dimen.h1_text_size));
-                } else {
-                    textView.setTextColor(mContext.getResources().getColor(R.color.white));
-                    textView.setTextSize(mContext.getResources().getDimension(R.dimen.h2_text_size));
+            public void onClick(View v) {
+                {
+                    selectedPosition.setVisibility(View.INVISIBLE);
+                    selectPrompt.setVisibility(View.INVISIBLE);
+                    confirmBtn.setVisibility(View.INVISIBLE);
+                    cancelBtn.setVisibility(View.INVISIBLE);
+                    selectedPositionTitle.setVisibility(View.INVISIBLE);
                 }
+                selectedAreaPositon = 0;
+                selectedAreaTextView.setTextColor(mContext.getResources().getColor(R.color.white));
+                selectedAreaTextView.setTextSize(mContext.getResources().getDimension(R.dimen.h2_text_size));
+
             }
         });
+
+
         gridView.setAdapter(cityAdapter);
     }
 
