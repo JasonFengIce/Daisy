6b9ca8a97a522fd402c96dc5c78e9cfec78224db
fenghuibin <15370770697@163.com>
2015-09-19 12:18:47 +0800

    fix Bug #7115: [腾讯盒子[视云影院]登录注册没有置灰态

--- Daisy/src/tv/ismar/daisy/ui/activity/UserCenterActivity.java
+++ Daisy/src/tv/ismar/daisy/ui/activity/UserCenterActivity.java
@@ -522,10 +522,9 @@ public class UserCenterActivity extends BaseActivity implements View.OnClickList
                 textFocusImage.setVisibility(View.INVISIBLE);
                 break;
             case Disable:
-                textView.setTextColor(getResources().getColor(R.color.personinfo_login_button_disable));
                 textSelectImage.setVisibility(View.INVISIBLE);
                 textFocusImage.setVisibility(View.INVISIBLE);
-//                parentView.setBackgroundResource(R.drawable.button_disable);
+                parentView.setBackgroundColor(R.color.personinfo_login_button_disable);
                 parentView.setFocusable(false);
                 parentView.setFocusableInTouchMode(false);
                 break;
