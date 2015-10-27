dff123e7e44799f1bf4c27cc60a99e621eb46653
pengzonghu <pengzonghu@ismartv.cn>
2015-10-27 11:45:16 +0800

    修改启动方式;解决launch页面进出视云影院流程问题.

--- Daisy/src/tv/ismar/daisy/ChannelListActivity.java
+++ Daisy/src/tv/ismar/daisy/ChannelListActivity.java
@@ -90,6 +90,71 @@ public class ChannelListActivity extends BaseActivity {
 		DaisyUtils.getVodApplication(this).addActivityToPool(this.toString(), this);
 	}
 
+	 @Override
+	    protected void onNewIntent(Intent intent) {
+	        super.onNewIntent(intent);
+	        setIntent(intent);
+	        setContentView(R.layout.channel_layout);
+			String title = null;
+			String url = null;
+			String channel = null;
+			int portraitflag =1;
+			if(intent!=null){
+				Bundle bundle = intent.getExtras();
+				if(bundle!=null){
+					url =bundle.getString("url");
+					
+					title = bundle.getString("title");
+					
+					channel = bundle.getString("channel");
+					portraitflag = bundle.getInt("portraitflag");
+				}
+			}
+			if(url==null) {
+				//url = "http://cord.tvxio.com/api/tv/sections/chinesemovie/";
+				url = SimpleRestClient.root_url+"/api/tv/sections/chinesemovie/";
+			//	url = "http://cord.tvxio.com/api/live/channel/movie/";
+			}
+			if(title==null) {
+				title = "华语电影";
+			}
+			if(channel==null) {
+				channel = "$histories_dd";
+			}
+			FragmentManager fragmentManager = getFragmentManager();
+			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+			if(channel!=null) {
+				if(channel.equals("$bookmarks")) {
+					Fragment favoriteFragment = new FavoriteFragment();
+					fragmentTransaction.add(R.id.fragment_container, favoriteFragment);
+				} else if(channel.equals("histories")) {
+					Fragment historyFragment = new HistoryFragment();
+					fragmentTransaction.add(R.id.fragment_container, historyFragment);
+				} 
+				else if(channel.equals("search")){
+					Intent searchIntent = new Intent();
+					searchIntent.setClass(ChannelListActivity.this, SearchActivity.class);
+					startActivity(searchIntent);
+					finish();
+				}
+				else {
+					channelFragment = new ChannelFragment();
+					if(1 == portraitflag){
+	                channelFragment.setIsPOrtrait(false);
+					}else if(2 == portraitflag){
+						channelFragment.setIsPOrtrait(true);					
+					}
+					channelFragment.mChannel = channel;
+					channelFragment.mTitle = title;  //chinesemovie
+	             
+	                channelFragment.mUrl = url;
+					fragmentTransaction.add(R.id.fragment_container, channelFragment);
+				}
+				fragmentTransaction.commit();
+			}
+			
+			DaisyUtils.getVodApplication(this).addActivityToPool(this.toString(), this);
+	 }
 	@Override
 	protected void onDestroy() {
 //		System.exit(0);
