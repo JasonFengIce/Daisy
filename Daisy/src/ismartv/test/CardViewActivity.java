5fe6acaf41b4cf80432159829600322666ecb8e3
fenghuibin <fenghuibin@ismartv.cn>
2015-10-30 11:56:39 +0800

    添加CardView控件

---
+++ Daisy/src/ismartv/test/CardViewActivity.java
@@ -0,0 +1,17 @@
+package ismartv.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import tv.ismar.daisy.R;
+
+/**
+ * Created by huaijie on 10/30/15.
+ */
+public class CardViewActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_activity_cardview);
+    }
+}
