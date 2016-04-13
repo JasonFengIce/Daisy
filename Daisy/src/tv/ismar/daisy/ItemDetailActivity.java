89aacaaec3ea068d998d50275562e761d295b529
cocojiajia <18701098956@139.com>
2016-04-13 14:13:43 +0800

    修复少儿频道购买后点击进入详情页崩溃的问题

--- Daisy/src/tv/ismar/daisy/ItemDetailActivity.java
+++ Daisy/src/tv/ismar/daisy/ItemDetailActivity.java
@@ -697,10 +697,10 @@ public class ItemDetailActivity extends BaseActivity implements
         // Set the content to Introduction View
         mDetailIntro.setText(mItem.description);
         // Set the favorite button's label.
-        if (isFavorite()) {
+        if (isFavorite()&&mCollectBtn!=null) {
             //mCollectBtn.setBackgroundResource(R.drawable.collected_btn_bg_selector);
             mCollectBtn.setText(getResources().getString(R.string.favorited));
-        } else {
+        } else if(mCollectBtn!=null){
             //mCollectBtn.setBackgroundResource(R.drawable.collect_btn_bg_selector);
             mCollectBtn.setText(getResources().getString(R.string.favorite));
         }
