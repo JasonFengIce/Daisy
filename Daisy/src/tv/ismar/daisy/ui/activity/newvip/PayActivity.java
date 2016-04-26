67f56d680295fe3b0b13a4f8190f058fca44812d
pengzonghu <pengzonghu@ismartv.cn>
2016-04-26 17:19:13 +0800

    修改测试PayActivity测试默认id.

--- Daisy/src/tv/ismar/daisy/ui/activity/newvip/PayActivity.java
+++ Daisy/src/tv/ismar/daisy/ui/activity/newvip/PayActivity.java
@@ -50,8 +50,8 @@ public class PayActivity extends BaseActivity implements View.OnHoverListener, V
         initViews();
         Intent intent = getIntent();
         String itemId = intent.getStringExtra("item_id");
-//        payLayer(itemId);
-        payLayer("675300");
+        payLayer(itemId);
+//        payLayer("675300");
     }
 
     private void initViews() {
