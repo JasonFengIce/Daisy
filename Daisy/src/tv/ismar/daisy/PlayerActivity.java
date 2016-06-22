16c80df4523d25f7a977f8f08bb652a7fcb659cc
joyopeng <joyopeng@gmail.com>
2016-06-23 07:40:40 +0800

    修改videolock调起逻辑.

--- Daisy/src/tv/ismar/daisy/PlayerActivity.java
+++ Daisy/src/tv/ismar/daisy/PlayerActivity.java
@@ -221,15 +221,26 @@ public class PlayerActivity extends VodMenuAction implements OnItemSelectedListe
 	}
 
 
-
+	@Override
+	public void setContentView(int layoutResID) {
+		super.setContentView(layoutResID);
+	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		if (needOnresume && isneedpause) {
-			isBuffer = true;
-			showBuffer();
-			initPlayer();
+        if(item == null){
+			String testitemurl = "http://sky.tvxio.bestv.com.cn/v3_0/UF30/tou/api/item/701893/";
+			if(testitemurl != null && !"".equals(testitemurl)){
+				GetItemTask mGetItemTask = new GetItemTask();
+				mGetItemTask.execute(testitemurl);
+			}
+		}else {
+			if (needOnresume && isneedpause) {
+				isBuffer = true;
+				showBuffer();
+				initPlayer();
+			}
 		}
 		isneedpause = true;
 		needOnresume = true;
@@ -386,7 +397,7 @@ public class PlayerActivity extends VodMenuAction implements OnItemSelectedListe
 		setVideoActionListener();
 		if("false".equals(shardpref.getSharedPrefs(AccountSharedPrefs.FIRST_USE))){
 			Intent intent = getIntent();
-			String testitemurl = " http://sky.tvxio.bestv.com.cn/v3_0/UF30/tou/api/item/701893/";
+			String testitemurl = "http://sky.tvxio.bestv.com.cn/v3_0/UF30/tou/api/item/701893/";
 			if(testitemurl != null && !"".equals(testitemurl)){
 				GetItemTask mGetItemTask = new GetItemTask();
 				mGetItemTask.execute(testitemurl);
