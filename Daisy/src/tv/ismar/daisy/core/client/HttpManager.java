8ee9d203fa29762f1e7140e20412f8cc93a9d824
fenghuibin <fenghuibin@ismartv.cn>
2016-07-04 13:08:10 +0800

    Fix Bug # delete http read timeout

--- Daisy/src/tv/ismar/daisy/core/client/HttpManager.java
+++ Daisy/src/tv/ismar/daisy/core/client/HttpManager.java
@@ -16,8 +16,7 @@ import retrofit2.Retrofit;
  * Created by huaijie on 1/18/16.
  */
 public class HttpManager {
-    private static final int DEFAULT_CONNECT_TIMEOUT = 2;
-    private static final int DEFAULT_READ_TIMEOUT = 5;
+    private static final int DEFAULT_CONNECT_TIMEOUT = 5;
 
     private static final String SKY_HOST = "http://media.lily.tvxio.com";
 
@@ -43,7 +42,6 @@ public class HttpManager {
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         mClient = new OkHttpClient.Builder()
                 .connectTimeout(DEFAULT_CONNECT_TIMEOUT, TimeUnit.SECONDS)
-                .readTimeout(DEFAULT_READ_TIMEOUT, TimeUnit.SECONDS)
                 .addInterceptor(interceptor)
                 .build();
         media_lily_Retrofit = new Retrofit.Builder()
@@ -56,7 +54,6 @@ public class HttpManager {
         Cache cache = new Cache(cacheFile, 1024 * 1024 * 100); //100Mb
         mCacheClient = new OkHttpClient.Builder()
                 .connectTimeout(DEFAULT_CONNECT_TIMEOUT, TimeUnit.SECONDS)
-                .readTimeout(DEFAULT_READ_TIMEOUT, TimeUnit.SECONDS)
                 .addInterceptor(interceptor)
                 .addInterceptor(CACHE_CONTROL_INTERCEPTOR)
                 .addNetworkInterceptor(CACHE_CONTROL_INTERCEPTOR)
